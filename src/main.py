54cd67299709641a71b21983c857a922992035d2
Firefret <firefret@proton.me>
2025-12-05 00:10:05 +0200

    deploy script

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os
 import shutil
+import sys
 from parsing_block import *
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, "../static")
-PUBLIC_DIR = os.path.join(BASE_DIR, "../public")
+PUBLIC_DIR = os.path.join(BASE_DIR, "../docs")
 TEMPLATE_PATH = os.path.join(BASE_DIR, "../template.html")
 MARKDOWN_PATH = os.path.join(BASE_DIR, "../content")
+
+website_path = sys.argv[1] if len(sys.argv ) > 1 else "/"
 
 def source_to_destination_copy(source = STATIC_DIR, destination = PUBLIC_DIR):
     source = os.path.realpath(source)
@@ -51,9 +54,13 @@ def generate_page(from_path: os.PathLike = MARKDOWN_PATH, template_path: os.Path
     file_name = "".join(file_name[:-1])
     print(f"File name: {file_name}.md")
     template = open(template_path, "r").read()
+
     html_string = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     output = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    output = output.replace("href=\"/", f"href=\"{website_path}")
+    output = output.replace("src=\"/", f"src=\"{website_path}")
+
     if not os.path.exists(dest_path):
         print(f"Destination directory not found, creating {dest_path}...")
         os.makedirs(dest_path)
@@ -62,6 +69,7 @@ def generate_page(from_path: os.PathLike = MARKDOWN_PATH, template_path: os.Path
     if os.path.exists(html_file_path):
         print(f"Removing existing file {file_name}.html...")
         os.remove(html_file_path)
+
     html_file = open(html_file_path, "x")
     html_file.write(output)
     print(f"Page {html_file_path} generated successfully!")
